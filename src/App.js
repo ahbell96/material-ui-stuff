4d41de445f547176468fdaf3c4d948eb114e7434
Andrew Bell <andrewbell545@gmail.com>
2021-11-27 19:49:49 +0000

    initial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Notes from './pages/Notes'
+import Create from './pages/Create'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Notes />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Notes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function Notes() {
+  return (
+    <div>
+      <p>Notes Page</p>
+      <Typography variant='h2'> Hello roboto test </Typography>
+      <Typography variant='body2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, minus repellat reprehenderit hic doloremque inventore error quas eius deserunt maxime odit nesciunt officiis rem ab earum itaque! Voluptatem, placeat culpa?</Typography>
+      <Button variant='contained'>Hello</Button>
+    </div>
+  )
+}
