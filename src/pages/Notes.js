c06af0290a3bef020e2db23cfc1801894caee75a
Andrew <andrewbell545@gmail.com>
2021-11-27 23:40:44 +0000

    playing around with some material ui

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,14 +1,48 @@
-import React from 'react'
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import SendAndArchiveOutlinedIcon from "@mui/icons-material/SendAndArchiveOutlined";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: "green",
+    "&:hover": {
+      backgroundColor: "darkgreen",
+    },
+  },
+  title: {
+    textDecoration: "underline",
+    marginBottom: 20,
+  },
+});
 
 export default function Notes() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <p>Notes Page</p>
-      <Typography variant='h2'> Hello roboto test </Typography>
-      <Typography variant='body2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, minus repellat reprehenderit hic doloremque inventore error quas eius deserunt maxime odit nesciunt officiis rem ab earum itaque! Voluptatem, placeat culpa?</Typography>
-      <Button variant='contained'>Hello</Button>
-    </div>
-  )
+    <Container>
+      <Typography
+        className={classes.title}
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+      >
+        Create a new note
+      </Typography>
+
+      <Button
+        className={classes.btn}
+        type="submit"
+        variant="contained"
+        color="secondary"
+        onClick={() => console.log("clicked")}
+        startIcon={<SendAndArchiveOutlinedIcon />}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
